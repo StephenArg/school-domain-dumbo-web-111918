cff78e8aee33ad4163916c7a02c5429e5bc5c6e0
Stephen Argentina <churchman1993@yahoo.com>
2018-11-22 23:21:52 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  def roster
+    @@students
+  end
+
+  def initialize(name)
+    @name = name
+    @@students = {}
+  end
+
+  def add_student(student, grade)
+    if @@students[grade]
+      @@students[grade].push(student)
+    else
+      @@students[grade] = [student]
+    end
+  end
+
+  def grade(grade)
+    @@students[grade]
+  end
+
+  def sort
+    @@students.each do |key, value|
+      value.sort!
+    end
+    @@students
+  end
+end
+
